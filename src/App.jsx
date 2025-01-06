f9785c022bd184cbb5a9f7b54567b2b397b23de4
Batuhan Yildirim <bthn.yildirim@gmail.com>
2025-01-06 18:23:24 +0100

    render json file in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./App.css";
+import foodsJson from "./foods.json";
 
 function App() {
+  const [foods, setFoods] = useState(foodsJsons);
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
