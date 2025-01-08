854e0e8ffb6522d20d5548311a02259a888789fc
Batuhan Yildirim <bthn.yildirim@gmail.com>
2025-01-08 16:31:10 +0100

    add fooxbox card and render in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import foodsJson from "./foods.json";
+import FoodBox from "./components/FoodBox";
 
 function App() {
-  const [foods, setFoods] = useState(foodsJsons);
+  const [foods, setFoods] = useState(foodsJson);
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <div>
+        {foods.map((food, index) => (
+          <FoodBox key={index} food={food} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,22 @@
-// Your code here
+import React from "react";
+
+function FoodBox({ food }) {
+  const { name, calories, image, servings } = food;
+
+  const totalCalories = calories * servings;
+
+  return (
+    <div className="food-card">
+      <p>{name}</p>
+      <img src={image} alt={name} />
+      <p>Calories: {calories}</p>
+      <p>Servings: {servings}</p>
+      <p>
+        <b>Total Calories: {totalCalories} kcal</b>
+      </p>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default FoodBox;
